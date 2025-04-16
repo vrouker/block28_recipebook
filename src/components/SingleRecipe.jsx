82d4256ea16bb6f67e98dfac17f7c86d3053288e
vrouker <vrouker@gmail.com>
2025-04-16 18:03:32 -0400

    added "add to favorites" button to single recipe view with conditional rendering

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -2,7 +2,7 @@
 
 function SingleRecipe({singleRecipe, setSingleRecipe}){
 
-
+const token = localStorage.getItem("token")
     return(
         <>
             <h1>{singleRecipe.strMeal}</h1>
@@ -12,6 +12,9 @@ function SingleRecipe({singleRecipe, setSingleRecipe}){
             <h2>Cooking Instructions:</h2>
             <p>{singleRecipe.strInstructions}</p>
             <button>Link to How To video</button>
+            {
+                token ? <button>Add to Favorites!</button> : null
+            }
         </>
     )
 }
