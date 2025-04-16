22aec396291d15bc58058d3e19c5e4debc4a752c
vrouker <vrouker@gmail.com>
2025-04-15 20:56:40 -0400

    created the single view of a recipe and linked to home page

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -1,10 +1,20 @@
 
 
-function SingleRecipe(){
+function SingleRecipe({singleRecipe, setSingleRecipe}){
+
+
     return(
         <>
-        
+            <h1>{singleRecipe.strMeal}</h1>
+            <p>{singleRecipe.strArea}</p>
+            <h2>Ingredients:</h2>
+            <p>{singleRecipe.ingredients}</p>
+            <h2>Cooking Instructions:</h2>
+            <p>{singleRecipe.strInstructions}</p>
+            <button>Link to How To video</button>
         </>
     )
 }
+
+//page does not keep the meal id and does not render when refreshed
 export default SingleRecipe
